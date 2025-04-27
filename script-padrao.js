96df13f3e98c39aa3f31cc2724e613d6f9ec237b
Geovanna Lessa <geovannalessa2@gmail.com>
2025-04-27 07:50:19 -0300

    Area de projeto Gfin - modo claro pronto

--- script-padrao.js
+++ script-padrao.js
@@ -16,20 +16,45 @@ window.addEventListener("load", function() {
     }
   
     btn.addEventListener('click', mudaIdioma);
+//colocar o js que tem em telas de projetos em outro arquivo js
+    const carrossel = document.getElementById('carrossel');
+    const btnProximo = document.getElementById('proximo');
+    const btnAnterior = document.getElementById('anterior');
+    
+    let index = 0;
+    const imagens = document.querySelectorAll('.carrossel-img');
+    
+    function mostrarImagem(i) {
+      const largura = carrossel.offsetWidth;
+      carrossel.scrollTo({ left: i * largura, behavior: 'smooth' });
+    }
+    
+    btnProximo.addEventListener('click', () => {
+      index = (index + 1) % imagens.length;
+      mostrarImagem(index);
+    });
+    
+    btnAnterior.addEventListener('click', () => {
+      index = (index - 1 + imagens.length) % imagens.length;
+      mostrarImagem(index);
+    });
 });
-
-
-
-
 
 function atualizarImagensTema() {
     const modoEscuro = document.body.classList.contains('dark-mode');
     const imagens = document.querySelectorAll('.imagem-projeto');
-  
+    const carrossel = document.querySelectorAll('.carrossel-img');
+
     imagens.forEach(img => {
       const novaSrc = modoEscuro ? img.dataset.dark : img.dataset.light;
       img.src = novaSrc;
     });
+  
+    carrossel.forEach(img => {
+      const novaSrc = modoEscuro ? img.dataset.dark : img.dataset.light;
+      img.src = novaSrc;
+    });
+   
   }
 
 function mudaTema(btn){
@@ -37,5 +62,5 @@ function mudaTema(btn){
     atualizarImagensTema();
 }
 
-let idiomaAtual = 'pt';
+
 
